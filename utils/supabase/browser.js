b8cc4afa29044059045f8cee302a83a2e6835baa
rslonee <rslonee@gmail.com>
2025-05-11 18:09:15 -0500

    Update browser.js

--- utils/supabase/browser.js
+++ utils/supabase/browser.js
@@ -1,8 +1,4 @@
-import { createBrowserClient } from '@supabase/ssr'
+// utils/supabase/browser.js
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 
-export function createBrowserSupabase() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  )
-}
+export const supabase = createBrowserSupabaseClient()
