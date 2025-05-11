e64d7152ef63739aa526746827f6671669462120
rslonee <rslonee@gmail.com>
2025-05-11 18:15:28 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,6 @@ export async function middleware(req: NextRequest) {
     data: { session }
   } = await supabase.auth.getSession()
 
-  // If no session, redirect to /login
   if (!session) {
     const loginUrl = new URL('/login', req.url)
     return NextResponse.redirect(loginUrl)
@@ -18,7 +17,6 @@ export async function middleware(req: NextRequest) {
   return res
 }
 
-// Only run on all routes except Next.js internals, static, api, and the login page
 export const config = {
   matcher: '/((?!_next/static|_next/image|favicon.ico|api|login).*)'
 }
