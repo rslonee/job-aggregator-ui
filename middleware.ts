81ea7002fe12f2ea85699be1d4e80ce8ddfed9b2
rslonee <rslonee@gmail.com>
2025-05-11 20:46:17 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,29 @@
 // middleware.ts
 
-import { NextRequest, NextResponse } from 'next/server'
-import { createMiddlewareClient } from '@supabase/ssr'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 
 export async function middleware(req: NextRequest) {
+  // create the Supabase CLient passing in the request
   const res = NextResponse.next()
-
-  // This will read/set the Supabase auth cookie
   const supabase = createMiddlewareClient({ req, res })
-  await supabase.auth.getSession()
+
+  // e.g. enforce authentication on /dashboard/*
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+    // redirect to login if no session
+    const loginUrl = new URL('/login', req.url)
+    return NextResponse.redirect(loginUrl)
+  }
 
   return res
 }
 
-// (Optional) apply to all routes except static assets
+// which routes should be protected by this middleware?
 export const config = {
-  matcher: ['/((?!_next/static).*)'],
+  matcher: ['/dashboard/:path*'],
 }
